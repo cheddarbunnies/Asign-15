edbbbd8c6dd37e490fd42451b4e7b0f6a1c34c60
cheddarbunny <bunny@bunny.com>
2021-12-05 17:48:23 -0500

    Range & Case exceptions and tests

--- Asign 15/Main.cpp
+++ Asign 15/Main.cpp
@@ -6,6 +6,16 @@ class Invalid_Character_Exception {
 
 };
 
+//Invalid range exception class
+class Invalid_Range_Exception {
+
+};
+
+//Invalid case exception case
+class Invalid_Case_Exception {
+
+};
+
 //Character Function
 char character(char start, int offset)
 {
@@ -16,21 +26,37 @@ char character(char start, int offset)
     try {
         if (startChar < 65 || (startChar > 90 && startChar < 97) || output > 122)
         {
-            throw Invalid_Character_Exception();
+            throw Invalid_Character_Exception(); //Throw
+        }
+        else if (output < 65 || (output > 90 && output < 97) || output > 122)
+        {
+            throw Invalid_Range_Exception(); //Throw
+        }
+        else if ((output >= 65 && output <= 90 * startChar >= 95 && startChar <= 122) || (output >= 95 && output <= 122 && startChar >= 65 && startChar <= 90))
+        {
+            throw Invalid_Case_Exception(); //Throw
         }
     }
 
     //Catch block
     catch (Invalid_Character_Exception) {
         //Display exception message if character is invalid
-        cout << "Invalid Character Exception" << endl;
+        cout << "Not a valid alphebet letter";
+        return 0;
     }
-
-    //Return alphabet letter
+    catch (Invalid_Range_Exception) {
+        //Display exception message if character not in range
+        cout << "Not a valid alphebet letter in range";
+        return 0;
+    }
+    catch (Invalid_Case_Exception) {
+        //Display incorrect case messange
+        cout << "Not a valid case";
+        return 0;
+    }
+    //Return alphabet letter if not an exception
     return char(output);
 }
-    
-
 
 //Main Function
 int main()
@@ -38,4 +64,7 @@ int main()
     //Test drivers
     cout << character('a', 3) << endl;
     cout << character('?', 1) << endl;
+    cout << character('a', -2) << endl;
+    cout << character('Z', -20) << endl;
+    cout << character('A', 32) << endl;
 }
